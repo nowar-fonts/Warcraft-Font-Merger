4d4b1f520b57bd846b0a4ccc99fda2ab77d59c41
Cyano Hao <c@cyano.cn>
2019-01-30 21:18:21 +0800

    fix filename in batch file

--- script-windows/merge+comp.bat
+++ script-windows/merge+comp.bat
@@ -1,9 +1,9 @@
 %~d0
 cd "%~dp0"
 
-.\otfccdump.exe --ignore-hints -o base.otd "%1"
+.\otfccdump.exe --ignore-hints -o base.otd %1
 .\otfccdump.exe --ignore-hints -o latin.otd latin.ttf
-.\otfccdump.exe --ignore-hints -o ext.otd "%2"
+.\otfccdump.exe --ignore-hints -o ext.otd %2
 .\otfccdump.exe --ignore-hints --name-by-hash -o cjk.otd cjk.ttf
 
 .\merge-otd.exe base.otd latin.otd ext.otd cjk.otd
--- script-windows/pack-zh.bat
+++ script-windows/pack-zh.bat
@@ -6,23 +6,23 @@ cd "%~dp0"
 if exist Fonts rmdir /s /q Fonts
 mkdir Fonts
 
-copy "%1" Fonts\FRIZQT__.ttf
-copy "%1" Fonts\ARIALN.ttf
-copy "%1" Fonts\skurri.ttf
-copy "%1" Fonts\MORPHEUS.ttf
+copy %1 Fonts\FRIZQT__.ttf
+copy %1 Fonts\ARIALN.ttf
+copy %1 Fonts\skurri.ttf
+copy %1 Fonts\MORPHEUS.ttf
 
-copy "%1" Fonts\FRIZQT___CYR.ttf
-copy "%1" Fonts\MORPHEUS_CYR.ttf
-copy "%1" Fonts\SKURRI_CYR.ttf
+copy %1 Fonts\FRIZQT___CYR.ttf
+copy %1 Fonts\MORPHEUS_CYR.ttf
+copy %1 Fonts\SKURRI_CYR.ttf
 
-copy "%1" Fonts\ARHei.ttf
-copy "%1" Fonts\ARKai_C.ttf
-copy "%1" Fonts\ARKai_T.ttf
+copy %1 Fonts\ARHei.ttf
+copy %1 Fonts\ARKai_C.ttf
+copy %1 Fonts\ARKai_T.ttf
 
-copy "%1" Fonts\bHEI00M.ttf
-copy "%1" Fonts\bHEI01B.ttf
-copy "%1" Fonts\bKAI00M.ttf
-copy "%1" Fonts\blei00d.ttf
+copy %1 Fonts\bHEI00M.ttf
+copy %1 Fonts\bHEI01B.ttf
+copy %1 Fonts\bKAI00M.ttf
+copy %1 Fonts\blei00d.ttf
 
 @echo on
 pause
